456e561659b85fc679ea52ed13db47494854cb4c
Ted Mitka <tmitka64@gmail.com>
2019-01-06 12:22:42 -0600

    added images and sized them

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    console.log("im linked");
+    var redGem = document.getElementById("redGem");
+    
+    var blueGem = document.getElementById("blueGem");
+    
+    var whiteGem = document.getElementById("whiteGem");
+    
+    var darkRedGem = document.getElementById("darkRedGem");
+    
+});
