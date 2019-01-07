f105259f936baa04f6ab910bd66315290f4bcdfe
Ted Mitka <tmitka64@gmail.com>
2019-01-07 09:39:30 -0600

    add pseudocode

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,16 @@
 $(document).ready(function() {
-    console.log("im linked");
+    //when the game starts assign the user a score to target
+    var targetScore = 0;
+
+    //assign values to each crystal
+
+    //when a crystal is clicked update the user score with what the crystal value represents
+
+    //if the userScore equals the target score the user "wins", update the win count, reset the game
+
+    //if the userScore becomes greater than the target score, the user "loses" the game, update loses, reset game
+
+
     var redGem = document.getElementById("redGem");
     
     var blueGem = document.getElementById("blueGem");
